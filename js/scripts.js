35c8d924055c0d3a16b7fe8622dd82ff2c5c9ca7
joaopereira14 <joaopereira@MacBook-Pro-de-Joao-2.local>
2017-03-16 22:17:51 +0000

    Inicio

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    
+    $('.btn').on('click', function() {
+        text('hello');        
+        
+    });
+
+});
+
+
+
+
+
+//
+//$('.list li').on('click', function() {
+//        
+//        $(this).addClass('active');
+//        
+//        
+//    });
